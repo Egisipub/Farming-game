12fe0ef99839f87c65c6467b6b7a92b4bae6dbdf
Charlie emery <cte031611@gmail.com>
2026-08-09 13:31:37 -0400

    started coding the tilemap system

--- sketch.js
+++ sketch.js
@@ -1,7 +1,32 @@
+
+const numTilesX = 20; 
+const numTilesY = 10;
+
+
+
+level = [
+  "01010101010101010101",
+  "10101010101010101010",
+  "01010101010101010101",
+  "10101010101010101010",
+  "01010101010101010101",
+  "10101010101010101010",
+  "01010101010101010101",
+  "10101010101010101010",
+  "01010101010101010101",
+  "10101010101010101010"
+];
+
+function whatSquare(row, col) {
+  if (row >= 0 && row < numTilesY && col >= 0 && col < numTilesX) {
+    return level[row].charAt(col);
+  }
+}
 async function setup() {
-  createCanvas(400, 400);
+  createCanvas(1440, 720);
 }
 
 function draw() {
   background(220);
+
 }
